0ede796caefc5e122edc337a5c3c5c1dcb8a2e51
Kiran Immadi <118165697+kiranimmadi2@users.noreply.github.com>
2024-09-27 01:26:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector(".hamburger");
+    
     const navMenu = document.querySelector("nav ul");
 
     hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("active");
+        
         navMenu.classList.toggle("active");
     });
 
